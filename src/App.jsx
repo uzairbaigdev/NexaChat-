2da82d8a55cbd76923c0d38aa93f49f00bb266cb
uzairBaig395 <mirzauzair448@gmail.com>
2026-07-25 17:36:30 +0500

    NexaChat-Signup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+// import { Routes, Route } from 'react-router-dom';
+// import { Link } from 'react-router-dom'
+import Signup from './pages/signup/signup';
+
+function App() {
+  return (
+   <>
+    <Signup/>
+   </>
+    
+  );
+}
+
+export default App;
