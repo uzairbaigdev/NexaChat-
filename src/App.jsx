f07fbe419f9c24f3440bb6de7e8da04dfb01c6e4
sharjeelahmed617458-commits <sharjeelahmed617458@gmail.com>
2026-07-28 02:09:04 +0500

    update coddonelogin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css';
-// import { Routes, Route } from 'react-router-dom';
-// import { Link } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Signup from './pages/signup/signup';
+import Login from './pages/signup/login/login'; // Signup ke andar login folder ka path
 
 function App() {
   return (
-   <>
-    <Signup/>
-   </>
-    
+    <Routes>
+      {/* Root (/) par automatic login par redirect */}
+      <Route path='/' element={<Navigate to="/login" replace />} />
+
+      {/* App Routes */}
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/login' element={<Login />} />
+    </Routes>
   );
 }
 
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc,doc,setDoc, serverTimestamp } from "firebase/firestore";
-import { createUserWithEmailAndPassword, getAuth,GoogleAuthProvider, signInWithPopup, getAdditionalUserInfo} from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth,GoogleAuthProvider, signInWithPopup, getAdditionalUserInfo , signInWithEmailAndPassword} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDXzTB9OwqqFQSJ_2s3RClKZ1xxQ_uM2Z4",
@@ -18,5 +18,5 @@ const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
 export { db, collection, addDoc, createUserWithEmailAndPassword, auth,doc,setDoc,serverTimestamp,
-  provider,getAuth, signInWithPopup,GoogleAuthProvider, getAdditionalUserInfo  
+  provider,getAuth, signInWithPopup,GoogleAuthProvider, getAdditionalUserInfo , signInWithEmailAndPassword 
 };
